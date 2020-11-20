2f74656f142b9934524d2e5420417e8093a75b96
Adam Sheaffer <adam.e.sheaffer@gmail.com>
2020-11-20 14:24:46 -0600

    catch exceptions

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace TryCatch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Guess a number: ");
+            string input = Console.ReadLine();
+
+            while (true)
+            {
+                try
+                {
+                    // This line might throw a FormatException
+                    int inputNum = int.Parse(input);
+                    Console.WriteLine($"Your guess was {inputNum}");
+
+                    // This line might throw a DivideByZeroException
+                    Console.WriteLine(1000 / inputNum);
+
+                    // Everything was successful. Break out of the while loop
+                    break;
+                }
+                catch (FormatException)
+                {
+                    Console.WriteLine($"Not a valid number");
+                }
+                catch (DivideByZeroException)
+                {
+                    Console.WriteLine("Oh, and don't pick zero");
+                }
+            }
+
+        }
+    }
+}
